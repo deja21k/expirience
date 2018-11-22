1f2dc4cd7c79058f32c7981cb86c6deb71035c3a
deja21k <thedoctor92@hotmail.com>
2018-11-22 21:07:43 +0100

    big push

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
+
   $('.design__slider').slick({
+
     prevArrow: $('.prev'),
     nextArrow: $('.next'),
     slidesToShow: 1,
   });
-});
+
+  $('.community__slider').slick({
+    
+    prevArrow: $('.prev1'),
+    nextArrow: $('.next1'),
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+    // mobileFirst: true,
+
+    // centerMode: true,
+    // centerPadding: '20px'
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    });
+  });

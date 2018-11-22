b400da8deab284edb5adff01dbfcabc3b42efb17
Bojan Lazic <lazicbojanns@gmail.com>
2018-11-22 21:05:01 +0100

    Include mask

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
+
+  maska();
+
   $('.design__slider').slick({
     prevArrow: $('.prev'),
     nextArrow: $('.next'),
     slidesToShow: 1,
   });
+
+  function maska() {
+    $(document).keydown(function(event){
+      if ((event.ctrlKey || event.metaKey)  && event.which == 88){
+        $('#maska').toggle();
+      }
+    });
+  }
 });

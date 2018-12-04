f24413ca2398fdc883c3aee3e3f3db90a164ef59
Bojan Lazic <lazicbojanns@gmail.com>
2018-12-04 21:46:41 +0100

    Update overlay && slider community

--- js/main.js
+++ js/main.js
@@ -1,44 +1,32 @@
 $(document).ready(function(){
 
-  initCommunitySlider()
-  initDesignSlider();
-  maska();
+    initCommunitySlider()
+    initDesignSlider();
+    maska();
 
-function initCommunitySlider() {
-  $('.community__slider').slick({
+    function initCommunitySlider() {
+        $('.community__slider').slick({
+            prevArrow: $('.prev1'),
+            nextArrow: $('.next1'),
+            slidesToShow: 2,
+            centerPadding: 0,
+        });
 
-    prevArrow: $('.prev1'),
-    nextArrow: $('.next1'),
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding:10,
+    }
 
+    function initDesignSlider() {
+        $('.design__slider').slick({
+            prevArrow: $('.prev'),
+            nextArrow: $('.next'),
+            slidesToShow: 1,
+        });
+    }
 
-
-    // mobileFirst: true,
-    //
-    // centerMode: true,
-    // centerPadding: '20px'
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-  });
-
-}
-
-  function initDesignSlider() {
-  $('.design__slider').slick({
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
-    slidesToShow: 1,
-  });
-  }
-
-  function maska() {
-    $(document).keydown(function(event){
-      if ((event.ctrlKey || event.metaKey)  && event.which == 88){
-        $('#maska').toggle();
-      }
-    });
-  }
+    function maska() {
+        $(document).keydown(function(event){
+            if ((event.ctrlKey || event.metaKey)  && event.which == 88){
+                $('#maska').toggle();
+            }
+        });
+    }
 });
